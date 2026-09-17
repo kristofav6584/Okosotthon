21ea32fd49e5656acde75afa1d713289ac0f3c1e
kristofav6584 <kovacsk3@kkszki.hu>
2026-09-17 11:55:35 +0200

    PushErtesites osztály

--- Okosotthon/Models/PushErtesites.cs
+++ Okosotthon/Models/PushErtesites.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Okosotthon.Models
 {
-    internal class PushErtesites
+    public class PushErtesites : Ertesites
     {
+        public string EszkozId { get; set; }
+
+        public PushErtesites(string eszkozId)
+        {
+            EszkozId = eszkozId;
+        }
+
+        public override void Kuldes(string uzenet)
+        {
+            DateTime ido = DateTime.Now;
+            Console.WriteLine($"push - {EszkozId} {ido}: {uzenet}");
+        }
     }
 }
